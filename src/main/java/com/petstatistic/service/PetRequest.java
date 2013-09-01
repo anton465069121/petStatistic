81d3b788c780d9e65701cdfac1ecbe8f1b8da38b
张晓丰 <zxflimit@gmail.com>
2013-09-01 13:24:26 +0800

    增加忘记提交的文件

---
+++ src/main/java/com/petstatistic/service/PetRequest.java
@@ -0,0 +1,15 @@
+package com.petstatistic.service;
+
+import java.util.HashMap;
+
+public class PetRequest {
+	HashMap<String, Object> params;
+
+	public HashMap<String, Object> getParams() {
+		return params;
+	}
+
+	public void setParams(HashMap<String, Object> params) {
+		this.params = params;
+	}
+}
